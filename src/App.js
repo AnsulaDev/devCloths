9a8ad6772e69ad65db872a080af4891c8554758d
AnsulaDev <ansulaoari88@gmail.com>
2022-12-27 19:48:57 +0530

    updated home route page styles

--- src/App.js
+++ src/App.js
@@ -8,20 +8,27 @@ import Shop from './routes/shop/shop.component';
 
 
 
+
 const App = () => {
   return (
-    <Routes>
-      <Route path ='/' element={<Navigation />}>
-        <Route index element={ <Home />} />   
-        <Route path='shop/*'element={ <Shop /> } />
-        <Route path='auth'element={ <Authentication/> } />
-        <Route path='checkout'element={ <Checkout /> } />
-
-      </Route>
-      
-
-    </Routes>
     
+      
+    <Routes>
+    
+      <Route path ='/' element={<Navigation />}>
+      <Route index element={ <Home />} />   
+      <Route path='shop/*'element={ <Shop /> } />
+      <Route path='auth'element={ <Authentication/> } />
+      <Route path='checkout'element={ <Checkout /> } />
+
+    </Route>
+    
+
+  </Routes>
+  
+
+  
+  
   );
 
 };
