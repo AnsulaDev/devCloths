ae1a6756bf6d9375db793369986e8e1897893aea
AnsulaDev <ansulaoari88@gmail.com>
2023-03-07 22:07:26 +0530

    added memoization function in selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,9 +1,17 @@
-export const selectCategoriesMap = (state) => {
-    console.log('selector fired');
-    return state.categories.categories
-        .reduce((acc, catergory) => {
-        const {title, items} = catergory
-        acc[title.toLowerCase()] = items;
-        return acc;
-    }, {});
-}
+import {createSelector} from 'reselect';
+
+const selectCategoryReducer = (state) => state.categories;
+export const selectCategories = createSelector(
+    [selectCategoryReducer],  //if categories value doesnt changed then it going to be same
+
+    (categoriesSlice) => categoriesSlice.categories
+);
+export const selectCategoriesMap =  createSelector(
+    [selectCategories], //memoized
+    (categories)=>categories  
+    .reduce((acc, catergory) => { //we dont wanna reduce if categoreis doesnt changed
+    const {title, items} = catergory
+    acc[title.toLowerCase()] = items;
+    return acc;
+}, {})
+);
