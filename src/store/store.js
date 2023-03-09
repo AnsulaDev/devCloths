6bf51c3b0f11b7ce8fb10d255a2ab3fc23e8f1ea
AnsulaDev <ansulaoari88@gmail.com>
2023-03-09 21:21:24 +0530

    added redux persit library  and moved custom middleware  into its own folder

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,26 @@
 import{ compose, createStore, applyMiddleware} from 'redux';
-
-
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import {rootReducer } from './root-reducer';
-
-const loggerMiddleware = (store) => (next) => (action) =>{
-    if(!action.type){
-        return next(action);
-    }
-    console.log('type:', action.type);
-    console.log('payload:', action.payload);
-    console.log('currentState:', store.getState());
-
-    next(action);
-    console.log('next state:',store.getState());
-};
-
-const middleWares = [loggerMiddleware];
-
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+import logger from 'redux-logger';
 
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [process.env.NODE_ENV  !== 'production' && logger].filter(
+    Boolean
+);
+
+const composeEhancer =(process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composedEnhancers = composeEhancer(applyMiddleware(...middleWares));
+
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const  persistor = persistStore(store);
